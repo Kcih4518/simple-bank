860cdfb8b9355f1a7f92b2225bad96caf2ae959e
Avery Yang(楊長珈) <avery_yang@asus.com>
2022-09-27 11:26:15 +0800

    feat: add unit-test for TestCreateAccount

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "tom",
+		Balance:  10,
+		Currency: "USD",
+	}
+	// testQueries is declared in the main_test
+	// CreateAccount shouldn't return err and return account not empty
+	// account's data should equal arg
+	// account's ID and CreatedAt shouldn't be zero
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+}
