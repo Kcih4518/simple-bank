5fd483f2e099522066950565931d8deb7d8ec1c2
Avery Yang(楊長珈) <avery_yang@asus.com>
2022-09-27 11:40:32 +0800

    feat: add RandomString to generate string

--- util/random.go
+++ util/random.go
@@ -2,8 +2,11 @@ package util
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 // init() function will be called automatically when the package is first used.
 // if use rand.Seed(1) will use constant value
@@ -16,4 +19,17 @@ func init() {
 // min ~max
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63(max-min+1)
+}
+
+// RandomString generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
 }
