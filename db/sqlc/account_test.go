3dca78ab7eb7b23f0d915c404f3e5c923c72010a
Avery Yang(楊長珈) <avery_yang@asus.com>
2022-09-27 18:13:01 +0800

    feat: add unit-test for TestGetAccount

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/Kcih4518/simple-bank/util"
 	"github.com/stretchr/testify/require"
@@ -34,4 +35,17 @@ func createRandomAccount(t *testing.T) Account {
 
 func TestCreateAccount(t *testing.T) {
 	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
