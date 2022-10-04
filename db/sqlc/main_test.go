969f967e984325a09d0190a3669cd0697561e129
Avery Yang(楊長珈) <avery_yang@asus.com>
2022-10-04 14:08:58 +0800

    feat: modify TestMian to transfer *sql.DB

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -16,15 +16,17 @@ const (
 
 // Queries contain a `dbtx` : a db connection or a transaction
 var testQueries *Queries
+var testDB *sql.DB
 
 // the main entry point off all unit tests
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	// New is defined in the db.go
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	// m.Run(): star running unit-tests
 	os.Exit(m.Run())
