ec326664c2a450a434076682f49de8d1624f8f57
Avery Yang(楊長珈) <avery_yang@asus.com>
2022-10-04 20:33:11 +0800

    feat: add GetAccountForUpdate to get account and will stop other process

--- db/query/account.sql
+++ db/query/account.sql
@@ -17,6 +17,15 @@ FROM accounts
 WHERE id = $1
 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT *
+FROM accounts
+WHERE id = $1
+LIMIT 1
+FOR
+UPDATE;
+
+
 -- name: ListAccounts :many
 SELECT *
 FROM accounts
