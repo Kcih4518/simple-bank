4c861e0a135728c16a3b33439c37f3ed949541a1
Avery Yang(楊長珈) <avery_yang@asus.com>
2022-11-15 19:41:08 +0800

    feat: add Store interface for mockDB testing and modify NewStore for real db

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,24 +6,29 @@ import (
 	"fmt"
 )
 
+// Store defines all functions to execute db queries and transactions
+type Store interface {
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
 // Store provides all functions to execute SQL queries and transactions
 // Transaction has multi db queries but each query do 1 operation on specific table.
 // So queries struct doesn't support transaction, since we need to use Store struct to extend its functionality
-type Store struct {
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
 // NewStore creates a new store
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // execTx executes a function within a database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -60,7 +65,7 @@ type TransferTxResult struct {
 
 // TransferTx performs a money transfer from one account to the other.
 // It creates a transfer record, add account entries, add update account's balance within a single database transaction
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
