155f065d66200379356c40063bd2103f384896ce
Avery Yang(楊長珈) <avery_yang@asus.com>
2022-11-15 19:54:28 +0800

    feat: mockgen -destination db/mock/store.go -package mockdb github.com/Kcih4518/simple-bank/db/sqlc Store

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -10,7 +10,7 @@ import (
 
 type Querier interface {
 	AddAccountBalance(ctx context.Context, arg AddAccountBalanceParams) (Account, error)
-	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
+CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
 	CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry, error)
 	CreateTransfer(ctx context.Context, arg CreateTransferParams) (Transfer, error)
 	DeleteAccount(ctx context.Context, id int64) error
